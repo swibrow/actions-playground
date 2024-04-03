8a97f13bfaba2e553c3daf0d6060ba3dd7d7267d
Samuel <15628653+swibrow@users.noreply.github.com>
2024-04-03 08:34:18 +0200

    end to end workflow

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
-terraform {}
+terraform {
+  backend "s3" {
+    region = "eu-central-1"
+  }
+}
+
+resource "random_pet" "this" {
+  length = 2
+}
+
+resource "random_id" "this" {
+  byte_length = 4
+}
+
+output "random_pet" {
+  value = random_pet.this.id
+}
+
+output "random_id" {
+  value = random_id.this.id
+}
+
